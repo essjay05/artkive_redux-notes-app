9366a3fb17fde5f9752b88e39593f9621833a9af
Joy <stepjoyous@gmail.com>
2019-03-13 16:52:27 -0700

    Added persisted State mode to index.js fin trying to implement localstorage data.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './js/store/index';
+import notesApp from './js/reducers';
+import { loadState, saveState } from './localstorage';
 import './index.css';
 import App from './js/components/App.jsx';
 // import * as serviceWorker from './serviceWorker';
 // import { BrowserRouter as Router } from 'react-router-dom';
 // import index from './js/index';
+
+const persistedState = loadState();
+const store = createStore(
+    notesApp,
+    persistedState
+);
+
+store.subscribe(() => {
+    saveState(store.getState());
+})
 
 
 render(
